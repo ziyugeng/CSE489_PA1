5082ce49f31d8d2fbde3c96479f8080d5fe6a8f7
耿子逾 <gengziyu@gengziyudeMacBook-Pro-7.local>
2023-10-08 13:19:02 -0400

    .

---
+++ cse489589_assignment1/ziyugeng/include/client.h
@@ -0,0 +1,14 @@
+// client.h
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#define TRUE 1
+#define MSG_SIZE 256
+#define BUFFER_SIZE 256
+
+int connect_to_host(char *server_ip, char *server_port);
+
+void start_client(const char* ip, const char* port);
+
+#endif
+
