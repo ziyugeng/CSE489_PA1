9acb2f07f1821cbd7dbaec999266e912dd750480
耿子逾 <gengziyu@gengziyudeMacBook-Pro-7.local>
2023-10-09 00:49:44 -0400

    .

---
+++ CSE489_PA1/ziyugeng/src/client.c
@@ -0,0 +1,144 @@
+/**
+* @client
+* @author  Swetank Kumar Saha <swetankk@buffalo.edu>, Shivang Aggarwal <shivanga@buffalo.edu>
+* @version 1.0
+*
+* @section LICENSE
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details at
+* http://www.gnu.org/copyleft/gpl.html
+*
+* @section DESCRIPTION
+*
+* This file contains the client.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+
+#define TRUE 1
+#define MSG_SIZE 256
+#define BUFFER_SIZE 256
+
+int connect_to_host(char *server_ip, char *server_port);
+
+/**
+* main function
+*
+* @param  argc Number of arguments
+* @param  argv The argument list
+* @return 0 EXIT_SUCCESS
+*/
+int main(int argc, char **argv)
+{
+	if(argc != 3) {
+		printf("Usage:%s [ip] [port]\n", argv[0]);
+		exit(-1);
+	}
+	
+	int server;
+	server = connect_to_host(argv[1], argv[2]);
+	
+	while(TRUE){
+		printf("\n[PA1-Client@CSE489/589]$ ");
+		fflush(stdout);
+		
+		char *msg = (char*) malloc(sizeof(char)*MSG_SIZE);
+		memset(msg, '\0', MSG_SIZE);
+		if(fgets(msg, MSG_SIZE-1, stdin) == NULL) //Mind the newline character that will be written to msg
+			exit(-1);
+		
+		printf("I got: %s(size:%d chars)", msg, strlen(msg));
+		
+		printf("\nSENDing it to the remote server ... ");
+		if(send(server, msg, strlen(msg), 0) == strlen(msg))
+			printf("Done!\n");
+		fflush(stdout);
+		
+		/* Initialize buffer to receieve response */
+		char *buffer = (char*) malloc(sizeof(char)*BUFFER_SIZE);
+		memset(buffer, '\0', BUFFER_SIZE);
+		
+		if(recv(server, buffer, BUFFER_SIZE, 0) >= 0){
+			printf("Server responded: %s", buffer);
+			fflush(stdout);
+		}
+	}
+}
+
+int connect_to_host(char *server_ip, char* server_port)
+{
+	int fdsocket;
+	struct addrinfo hints, *res;
+
+	/* Set up hints structure */	
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	/* Fill up address structures */	
+	if (getaddrinfo(server_ip, server_port, &hints, &res) != 0)
+		perror("getaddrinfo failed");
+
+	/* Socket */
+	fdsocket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if(fdsocket < 0)
+		perror("Failed to create socket");
+	
+	/* Connect */
+	if(connect(fdsocket, res->ai_addr, res->ai_addrlen) < 0)
+		perror("Connect failed");
+	
+	freeaddrinfo(res);
+
+	return fdsocket;
+}
+
+void start_client(char* port_str) {
+    char* server_ip = "127.0.0.1";  // assuming localhost for demonstration
+
+    int server;
+    server = connect_to_host(server_ip, port_str);
+
+    while(TRUE){
+        printf("\n[PA1-Client@CSE489/589]$ ");
+        fflush(stdout);
+
+        char *msg = (char*) malloc(sizeof(char)*MSG_SIZE);
+        memset(msg, '\0', MSG_SIZE);
+        if(fgets(msg, MSG_SIZE-1, stdin) == NULL) 
+            exit(-1);
+
+        printf("I got: %s(size:%d chars)", msg, strlen(msg));
+
+        printf("\nSENDing it to the remote server ... ");
+        if(send(server, msg, strlen(msg), 0) == strlen(msg))
+            printf("Done!\n");
+        fflush(stdout);
+
+        char *buffer = (char*) malloc(sizeof(char)*BUFFER_SIZE);
+        memset(buffer, '\0', BUFFER_SIZE);
+
+        if(recv(server, buffer, BUFFER_SIZE, 0) >= 0){
+            printf("Server responded: %s", buffer);
+            fflush(stdout);
+        }
+
+        free(msg);
+        free(buffer);
+    }
+}
+// The above code refers to the pa1_demo_code-client.c
---
+++ CSE489_PA1/ziyugeng/src/ziyugeng_assignment1.c
@@ -0,0 +1,111 @@
+/**
+ * @ubitname_assignment1
+ * @author  Ziyu Geng <ziyugeng@buffalo.edu>, Tianrui Tu <ttu4@buffalo.edu>
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * This contains the main function. Add further description here....
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "../include/global.h"
+#include "../include/logger.h"
+#include "../include/server.h"
+#include "../include/client.h"
+
+void start_server(char* port);
+void start_client(char* port);
+
+/**
+ * main function
+ *
+ * @param  argc Number of arguments
+ * @param  argv The argument list
+ * @return 0 EXIT_SUCCESS
+ */
+int main(int argc, char **argv)
+{
+	if(argc != 3) {
+        fprintf(stderr, "Usage: %s <s/c> <port_number>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+	/*Init. Logger*/
+	cse4589_init_log(argv[2]);
+
+	/*Clear LOGFILE*/
+	fclose(fopen(LOGFILE, "w"));
+
+	/*Start Here*/
+	// 3.3 Running your program
+	// s - server, c - client, port number
+	int port = atoi(argv[2]);
+	if (argv[1][0] == 's'){
+		start_server(argv[2]);
+	}
+	else if(argv[1][0] == 'c'){
+		start_client(argv[2]);
+	}
+	else{
+		printf("Error: Please use 's' for server and 'c' for client.\n");
+	}
+	return 0;
+}
+
+void execute_command(char* cmd) {
+    char *command_str = strtok(cmd, " \n");
+
+    if(strcmp(command_str, "AUTHOR") == 0) {
+        author_command();
+    }
+    // else if(strcmp(command_str, "IP") == 0) {
+    //     ip_command();
+    // }
+    else {
+        printf("[%s:ERROR]\n", command_str);
+        printf("[%s:END]\n", command_str);
+    }
+}
+
+
+// void ip_command() {
+//     char* command_str = "IP";
+//     char* ip_addr = /* function to get IP address */;
+
+//     if(ip_addr != NULL) {
+//         cse4589_print_and_log("[%s:SUCCESS]\n", command_str);
+//         cse4589_print_and_log("IP:%s\n", ip_addr);
+//         cse4589_print_and_log("[%s:END]\n", command_str);
+//     }
+//     else {
+//         cse4589_print_and_log("[%s:ERROR]\n", command_str);
+//         cse4589_print_and_log("[%s:END]\n", command_str);
+//     }
+// }
+
+void author_command() {
+    char* command = "AUTHOR";
+    char* ubit = "ziyugeng";
+
+    cse4589_print_and_log("[%s:SUCCESS]\n", command);
+    cse4589_print_and_log("I, %s, have read and understood the course academic integrity policy.\n", ubit);
+    cse4589_print_and_log("[%s:END]\n", command);
+}
+
